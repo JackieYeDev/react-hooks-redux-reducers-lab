69c469b75b6a2dd0817d3d43678697dff901f751
Jackie <jye0325@gmail.com>
2022-11-10 13:18:52 -0500

    commit tree 8d6c9811aff4ada180a01f5b28ce5a40f112743a

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,7 @@
 const initialState = { numberOfPresents: 0 };
-
+let action = {
+  type: 'presents/increase',
+};
 export function managePresents(state = initialState, action) {
   // your code here
 }
